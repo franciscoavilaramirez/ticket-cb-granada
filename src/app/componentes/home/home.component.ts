9a62e1cd274531b87ef9761c926e153e156735e9
vazquezpaco98 <33289326+vazquezpaco98@users.noreply.github.com>
2023-12-18 14:45:56 +0100

    hay que probar el getProximosPartidos pero ya compila

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -11,6 +11,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { UpdateUserComponent } from '../update-user/update-user.component';
 
 import Swal from 'sweetalert2';
+import { of } from 'rxjs';
 
 
 @Component({
@@ -56,7 +57,25 @@ export class HomeComponent {
       this.partidos = data
       console.log('data', this.partidos);  });
 
+      this.partidos = this.partidos.sort((n1, n2) => {
+        if (n1.fecha.getTime() > n2.fecha.getTime()){
+          return 1;
+        }
+        if (n1.fecha.getTime() < n2.fecha.getTime()){
+          return -1;
+        }
 
+        return 0;
+        
+      })
+
+
+      
+      for (var index in this.partidos) {
+        if(this.partidos[index].fecha.getTime() < this.todayDate.getTime()){
+          return this.partidos.slice(+index - 3, +index);
+        }
+      }
 
   }
 
