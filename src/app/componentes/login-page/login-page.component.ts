f518a97fb48c4c31456be602cdaa78c446e5b732
pmochonu <pablo.mochon-ubina@t-systems.com>
2024-01-15 16:19:32 +0100

    add localstorage and success alert

--- src/app/componentes/login-page/login-page.component.ts
+++ src/app/componentes/login-page/login-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { LoginResponse } from './../../modelo/LoginResponse';
+import { LoginResponse } from '../../modelo/LoginResponse';
+import { environment } from '../../../enviroments/environment';
 
 @Component({
   selector: 'app-login-page',
@@ -11,19 +12,52 @@ import { LoginResponse } from './../../modelo/LoginResponse';
 export class LoginPageComponent implements OnInit {
   loginForm: FormGroup;
   errorMessage: string = '';
+  successMessage: string;
 
   constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      email: '',
-      password: ''
+      email: ['', Validators.required],
+      password: ['', Validators.required]
     });
   }
 
   onSubmit(): void {
+    if (this.loginForm.valid) {
+      // Lógica para manejar el envío del formulario correctamente
+      this.successMessage = 'Formulario enviado correctamente.';
+
+      // guardar sesion localstorage
+      localStorage.setItem('user', JSON.stringify(this.loginForm.value));
+      console.log("localstorage ==>",localStorage.getItem('user') || '{}');
+
+    // guardar un valor específico en el localstorage
+    let email = this.loginForm.get('email')?.value;
+    localStorage.setItem('userEmail', email);
+    console.log("localstorage email ==>",localStorage.getItem('userEmail'));
+
+    let userId = this.loginForm.get('user_id')?.value;
+    localStorage.setItem('userId', userId);
+    console.log("localstorage ID ==>",localStorage.getItem('userId'));
+    }
+
     const observer = {
-      next: (response: LoginResponse) => console.log('Success!', response),
+      next: (response: LoginResponse) => {
+        console.log('Success!', response);
+
+        let user= {
+          nombre: response.nombre,
+          apellido: response.apellido,
+          email: response.email,
+          user_id: response.user_id,
+          tickets: response.tickets,
+          isAdmin: response._admin
+        }
+        let user1 = JSON.stringify(user);
+        localStorage.setItem('user', user1);
+        console.log("localstorage ID ==>",localStorage.getItem('user'));
+      },
       error: (error: any) => {
         console.error('Error!', error);
         this.errorMessage = 'Error al iniciar sesión. Por favor, inténtalo de nuevo.';
@@ -31,6 +65,6 @@ export class LoginPageComponent implements OnInit {
 
     };
 
-    this.http.post<LoginResponse>('http://localhost:8080/cbgranada-api/v1/login', this.loginForm.value).subscribe(observer);
+    this.http.post<LoginResponse>(environment.apiUrl+'login', this.loginForm.value).subscribe(observer);
   }
 }
