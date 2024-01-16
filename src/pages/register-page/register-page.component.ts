d3e9742f90dc9df8cb9b6dda4c54dec2977e7b9e
pmochonu <pablo.mochon-ubina@t-systems.com>
2024-01-16 15:03:51 +0100

    add register-page component

---
+++ src/pages/register-page/register-page.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-register-page',
+  templateUrl: './register-page.component.html',
+  styleUrls: ['./register-page.component.scss']
+})
+export class RegisterPageComponent implements OnInit {
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      apellidos: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      repeatPassword: ['', Validators.required]
+    }, { validator: this.checkPasswords });
+  }
+
+  checkPasswords(group: FormGroup) {
+    let passControl = group.get('password');
+    let confirmPassControl = group.get('repeatPassword');
+
+    if (passControl && confirmPassControl) {
+      let pass = passControl.value;
+      let confirmPass = confirmPassControl.value;
+
+      return pass === confirmPass ? null : { notSame: true }
+    }
+
+    return { notSame: true };
+  }
+
+  register() {
+    if (this.registerForm.valid) {
+      this.http.post('http://localhost:8080/cbgranada-api/v1/addUser', this.registerForm.value).subscribe({
+        next: () => {
+          // Maneja la respuesta exitosa aquí
+        },
+        error: error => {
+          // Maneja el error aquí
+        }
+      });
+    }
+  }
+}
