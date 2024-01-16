cb9420685ea68bf2305fc46de17d721b6d676be9
pmochonu <pablo.mochon-ubina@t-systems.com>
2024-01-16 15:02:36 +0100

    add signup routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { ArchivosPDFComponent } from './componentes/archivos-pdf/archivos-pdf.component';
 import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { RegisterPageComponent } from '../pages/register-page/register-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path:'home', component: HomeComponent },
   { path: 'tickets', component: ArchivosPDFComponent},
-  { path: 'login', component: LoginPageComponent}
+  { path: 'login', component: LoginPageComponent},
+  { path: 'signup', component: RegisterPageComponent}
 ];
 
 @NgModule({
