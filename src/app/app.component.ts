0453fa9b39e9c43e792bccc584b314bb316aefa1
pmochonu <pablo.mochon-ubina@t-systems.com>
2024-01-25 09:24:18 +0100

    cerrar sesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  router: any;
+logout() {
+  console.log('logout');
+
+  // borrar usuario del localstorage
+  localStorage.removeItem('user');
+  // redirigir a la página de inicio
+  window.location.href = '/';
+}
   title = 'ticket-cb-granada';
 }
