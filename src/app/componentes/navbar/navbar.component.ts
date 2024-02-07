35715fc6cbc6fd928b99d4a28d18f5eb392e8776
Sergio Diaz <sergio.diaz-ramirez@t-systems.com>
2024-02-07 13:23:19 +0100

    navBar comprueba si es admin

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -1,10 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
   @Input() isAdmin = false
+  user: any;
+  ngOnInit(): void {
+    let userStr = localStorage.getItem('user');
+    if(userStr != null) {
+      let user = JSON.parse(userStr);
+      this.isAdmin = user.isAdmin
+      console.log(user)
+      console.log("isAdmin: " + user.isAdmin)
+    }
+  }
 }
