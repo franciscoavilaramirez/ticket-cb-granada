09c789d9f1d4c837a5094e7a9d21f19b44145d12
Sergio Diaz <sergio.diaz-ramirez@t-systems.com>
2024-02-08 13:20:16 +0100

    fix: se espera a la respuesta para modificar usuario

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl  } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Usuario } from '../../modelo/usuario';
 import { ApiService } from '../../service/api.service';
@@ -14,17 +14,17 @@ import { Router } from '@angular/router';
   styleUrl: './perfil.component.css'
 })
 export class PerfilComponent {
-  editarPerfil:FormGroup;
-  usuario : Usuario
+  editarPerfil: FormGroup;
+  usuario: Usuario
   idUsuario: number
   errorMessage: string = '';
 
-  constructor(private formBuilder: FormBuilder, private apiService : ApiService, private router:Router,  private http: HttpClient, public dialog: MatDialog){}
+  constructor(private formBuilder: FormBuilder, private apiService: ApiService, private router: Router, private http: HttpClient, public dialog: MatDialog) { }
 
-  
+
   ngOnInit() {
     let user = localStorage.getItem('user');
-    if(user == null)
+    if (user == null)
       this.idUsuario = -1
     else {
       let userJson = JSON.parse(user);
@@ -34,13 +34,13 @@ export class PerfilComponent {
     this.apiService.getUsuarioById(this.idUsuario).subscribe(
       usuario => {
         this.usuario = usuario
-        this.usuario.id = this.idUsuario+"";
-        console.log("usuario perfil: "+ JSON.stringify(this.usuario))
+        this.usuario.id = this.idUsuario + "";
+        console.log("usuario perfil: " + JSON.stringify(this.usuario))
         this.editarPerfil = this.formBuilder.group({
           nombre: [this.usuario.nombre, Validators.required],
           apellidos: [this.usuario.apellidos, Validators.required],
           email: [this.usuario.email, [Validators.required, Validators.email]],
-         
+
         });
       }
     )
@@ -56,21 +56,22 @@ export class PerfilComponent {
       confirmButtonText: 'Aceptar',
       denyButtonText: 'Cancelar',
       confirmButtonColor: '#36BF98',
-      denyButtonColor:'grey',
+      denyButtonColor: 'grey',
+    }).then((response) => {
+      if (response.isConfirmed) 
+        this.apiService.modifyUser(usuario).subscribe()
     });
-  
-    this.apiService.modifyUser(usuario).subscribe(()=>{})
   }
 
-  cambiarPassword(usuario:Usuario) {
+  cambiarPassword(usuario: Usuario) {
     usuario.id = this.idUsuario + "";
-    this.dialog.open(EditPasswordComponent,{
-      data: usuario ,
-      width:'450px',
-      height:'600px'
+    this.dialog.open(EditPasswordComponent, {
+      data: usuario,
+      width: '450px',
+      height: '600px'
     });
   }
-  
+
   cerrarSesion() {
     localStorage.removeItem('user')
     this.router.navigate(['/login']);
