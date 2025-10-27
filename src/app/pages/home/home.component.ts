d216cd5e5d43b646e7850e4d900c36e144e48706
Paul <paul-alexandru.stanescu@t-systems.com>
2025-10-27 09:52:53 +0100

    Traducciones y mejoras en pagina inicio

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,10 @@ import { ApiService } from '../../service/api.service';
 import { Partido } from '../../modelo/partido';
 import { MatDialog } from '@angular/material/dialog';
 import { AddEntradasUsuarioComponent } from '../../componentes/add-entradas-usuario/add-entradas-usuario.component';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { ThemePalette } from '@angular/material/core';
 import { UserService } from '../../service/user.service';
-
+import Swal from 'sweetalert2';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
@@ -15,93 +15,128 @@ import { UserService } from '../../service/user.service';
 })
 export class HomeComponent {
 
-  constructor(private apiService: ApiService, private renderer: Renderer2,public dialog: MatDialog,private userService:UserService, private cdr: ChangeDetectorRef) {
+  constructor(
+    private apiService: ApiService,
+    private renderer: Renderer2,
+    public dialog: MatDialog,
+    private userService: UserService,
+    private cdr: ChangeDetectorRef,
+    private translate: TranslateService
+  ) {}
 
-  }
-  idioma: string = 'es'; // valor inicial por defecto
-  idUsuario: number
-  partidos: Partido[]
-  misPartidosIds: number[]
-  primerPartido: Partido
+  idioma: string = 'es';
+  idUsuario: number;
+  partidos: Partido[] = [];
+  misPartidosIds: number[] = [];
+  primerPartido: Partido;
   filterPost = '';
-  partidosFuturos: Partido[] =[];
+  partidosFuturos: Partido[] = [];
   spinnerShow = false;
   color: ThemePalette = "accent";
   infoUserNoMatch: boolean = false;
 
-
   ngOnInit() {
+    this.cargarDatos();
+  }
 
-    this.spinnerShow = true
+  cargarDatos() {
+    this.spinnerShow = true;
     this.apiService.idioma$.subscribe((nuevoIdioma: string) => {
       this.idioma = nuevoIdioma;
     });
-    this.idUsuario = this.getUsuarioId()
-    this.apiService.getMisPartidosIds(this.idUsuario).subscribe(misPartidosIds => {
-      this.misPartidosIds = misPartidosIds
-      this.apiService.getProximosPartidos().subscribe(proximosPartidos => {
-        if (proximosPartidos != null) {
-          this.primerPartido = proximosPartidos[0]
-          this.spinnerShow = false;
-          if (this.misPartidosIds?.includes(this.primerPartido.id))
-            this.primerPartido.tengoEntrada = true
-          else
-            this.primerPartido.tengoEntrada = false
 
-          proximosPartidos.splice(0, 1) //quitamos el primer partido para mostrarlo mas grande como siguiente partido
-          this.partidos = proximosPartidos //el resto de partidos que se mostraran abajo mas pequeños
-          this.partidos.forEach(partido => {
-            if (this.misPartidosIds?.includes(partido.id))
-              partido.tengoEntrada = true;
-            else
-              partido.tengoEntrada = false;
-          })
-        }
-        if (!this.primerPartido) {
+    this.idUsuario = this.getUsuarioId();
+
+    this.apiService.getMisPartidosIds(this.idUsuario).subscribe(misPartidosIds => {
+      this.misPartidosIds = misPartidosIds;
+
+      this.apiService.getProximosPartidos().subscribe(proximosPartidos => {
+        if (proximosPartidos && proximosPartidos.length > 0) {
+          this.primerPartido = proximosPartidos[0];
+          this.spinnerShow = false;
+
+          // Marcar si tiene entrada
+          this.primerPartido.tengoEntrada = this.misPartidosIds.includes(this.primerPartido.id);
+
+          // Quitar el primero de la lista para mostrar aparte
+          proximosPartidos.splice(0, 1);
+          this.partidos = proximosPartidos.map(p => ({
+            ...p,
+            tengoEntrada: this.misPartidosIds.includes(p.id)
+          }));
+        } else {
           this.infoUserNoMatch = true;
           this.spinnerShow = false;
-          this.cdr.detectChanges();// Forzamos para que detecte cambios y asegurarnos de que muestra el mensaje al usuario
-          console.log('primer partido',this.primerPartido) 
-        }else {this.infoUserNoMatch = false
-          console.log('info user a false') 
-
+          this.cdr.detectChanges();
         }
       });
     });
+
     this.getPartidosFuturos();
   }
 
   apuntarse(idPartido: number) {
-    this.apiService.asignarEntrada(this.idUsuario, idPartido).subscribe(response => {
-      if (response) {
-        if (this.primerPartido.id == idPartido)
-          this.primerPartido.tengoEntrada = true
-        else {
-          this.partidos.forEach(partido => {
-            if (partido.id == idPartido)
-              partido.tengoEntrada = true;
-          });
-        }
-      } else {
-          alert("No quedan entradas")
-      }
-    })
+  this.apiService.asignarEntrada(this.idUsuario, idPartido).subscribe(response => {
+    if (response) {
+      this.actualizarPartidoLocal(idPartido, true);
+    } else {
+      // Usamos ngx-translate para los textos
+      const titulo = this.translate.instant('alertas.entradasAgotadasTitulo');
+      const texto = this.translate.instant('alertas.entradasAgotadasTexto');
+      const confirmar = this.translate.instant('alertas.aceptar');
 
-  }
+      Swal.fire({
+        title: titulo,
+        text: texto,
+        icon: 'warning',
+        confirmButtonText: confirmar,
+        confirmButtonColor: '#36BF98',
+        background: '#fff',
+        color: '#333',
+        showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
+      });
+    }
+  });
+}
+
   devolver(idPartido: number) {
     this.apiService.desasignarEntrada(this.idUsuario, idPartido).subscribe(() => {
-      if (this.primerPartido.id == idPartido) {
-        this.primerPartido.tengoEntrada = false
-      }
-      else {
-        this.partidos.forEach(partido => {
-          if (partido.id == idPartido) {
-            partido.tengoEntrada = false;
-          }
-        });
-      }
-    })
+      this.actualizarPartidoLocal(idPartido, false);
+    });
   }
+
+  private actualizarPartidoLocal(idPartido: number, tengoEntrada: boolean) {
+    // Actualiza primer partido
+    if (this.primerPartido?.id === idPartido) {
+      this.primerPartido.tengoEntrada = tengoEntrada;
+    }
+
+    // Actualiza el resto
+    this.partidos = this.partidos.map(p =>
+      p.id === idPartido ? { ...p, tengoEntrada } : p
+    );
+
+    // Actualiza los IDs de usuario
+    if (tengoEntrada) {
+      if (!this.misPartidosIds.includes(idPartido)) {
+        this.misPartidosIds.push(idPartido);
+      }
+    } else {
+      this.misPartidosIds = this.misPartidosIds.filter(id => id !== idPartido);
+    }
+
+    // Forzamos detección de cambios en la vista
+    this.cdr.detectChanges();
+  }
+
+  /**
+   * Descarga la entrada en PDF
+   */
   descargar(idPartido: number, nombrePartido: string) {
     this.apiService.getEntrada(this.idUsuario, idPartido).subscribe(entradaPdf => {
       entradaPdf.forEach(file => {
@@ -115,39 +150,37 @@ export class HomeComponent {
         const url = window.URL.createObjectURL(blob);
         const link = document.createElement('a');
         link.href = url;
-        link.download = 'Granada - ' + nombrePartido + '.pdf';
+        link.download = `Granada - ${nombrePartido}.pdf`;
         link.click();
         window.URL.revokeObjectURL(url);
-
-      })
-
+      });
     });
   }
 
   getUsuarioId(): number {
     const userId = this.userService.getUserData();
-     return userId.id;
+    return userId.id;
   }
 
   openAddTicketsUser() {
-    const dialog = this.dialog.open(AddEntradasUsuarioComponent,{
-      width:'30vw',
-      height:'60vh'
+    const dialog = this.dialog.open(AddEntradasUsuarioComponent, {
+      width: '30vw',
+      height: '60vh'
     });
-    dialog.afterClosed().subscribe(result => {
-      this.ngOnInit();
+    dialog.afterClosed().subscribe(() => {
+      this.cargarDatos();
     });
   }
-  getPartidosFuturos(){
-    this.apiService.getProximosPartidosDisponibles().subscribe(partidosFuturos =>{
+
+  getPartidosFuturos() {
+    this.apiService.getProximosPartidosDisponibles().subscribe(partidosFuturos => {
       this.partidosFuturos = partidosFuturos;
     });
   }
 
-  descargarVarias(idPartido: number, nombrePartido: string, contentType = '') {
+  descargarVarias(idPartido: number, nombrePartido: string) {
     this.apiService.getEntradasExtra(2, 1, 2).subscribe(entradaPdf => {
       entradaPdf.forEach(file => {
-        //console.log("aaaaaaaaa "+file.data + file.fileName)
         const byteCharacters = atob(file.data);
         const byteArrays = [];
 
@@ -159,11 +192,10 @@ export class HomeComponent {
         const url = window.URL.createObjectURL(blob);
         const link = document.createElement('a');
         link.href = url;
-        link.download = 'Granada - ' + nombrePartido + '.pdf';
+        link.download = `Granada - ${nombrePartido}.pdf`;
         link.click();
         window.URL.revokeObjectURL(url);
-
-      })
+      });
     });
   }
 }
