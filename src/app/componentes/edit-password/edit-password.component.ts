25138e4deac3058833610163c0bd39dda9fc16fd
Paul <paul-alexandru.stanescu@t-systems.com>
2025-10-27 09:47:46 +0100

    Optimización funcionalidad editar contraseña

--- src/app/componentes/edit-password/edit-password.component.ts
+++ src/app/componentes/edit-password/edit-password.component.ts
@@ -1,18 +1,24 @@
-import { Component, Inject, signal } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators, ValidatorFn, AbstractControl, ValidationErrors, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, Inject, OnInit, signal } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl
+} from '@angular/forms';
 import { ApiService } from '../../service/api.service';
-import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { Usuario } from '../../modelo/usuario';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { PerfilComponent } from '../../pages/perfil/perfil.component';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatButtonModule } from '@angular/material/button';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit-password',
@@ -32,70 +38,120 @@ import { MatButtonModule } from '@angular/material/button';
     MatButtonModule,
   ],
 })
-export class EditPasswordComponent {
-
-  editPassword: FormGroup;
-  hide = signal(true);
-  escondido = signal(true);
+export class EditPasswordComponent implements OnInit {
+  editPassword!: FormGroup;
   hidePassword: { [key: string]: boolean } = {
     actual: true,
     nueva: true,
-    repetida: true
+    repetida: true,
   };
+  private subs: Subscription[] = [];
 
-  constructor(private service: ApiService, public dialog: MatDialog, private formBuilder: FormBuilder,private matIconModule: MatIconModule,
+  private readonly passwordPattern =
+    /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~`]).{8,}$/;
+
+  constructor(
+    private service: ApiService,
+    public dialog: MatDialog,
+    private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<PerfilComponent>,
-    @Inject(MAT_DIALOG_DATA) public userModify: Usuario) { }
+    @Inject(MAT_DIALOG_DATA) public userModify: Usuario
+  ) {}
 
   ngOnInit() {
-    this.editPassword = this.formBuilder.group({
-      contrasenaActual: ['', [Validators.required, Validators.pattern("^(?=.*\\d)(?=.*[@#$%^&+=*_-])(?=.*[a-z])(?=.*[A-Z]).{8,}$")]],
-      contrasenaNueva: ['', [Validators.required, Validators.pattern("^(?=.*\\d)(?=.*[@#$%^&+=*_-])(?=.*[a-z])(?=.*[A-Z]).{8,}$")]],
-      contrasenaRepetida: ['', [Validators.required, Validators.pattern("^(?=.*\\d)(?=.*[@#$%^&+=*_-])(?=.*[a-z])(?=.*[A-Z]).{8,}$")]]
-    }, { validator: this.checkPasswordsMatch });
+    this.editPassword = this.formBuilder.group(
+      {
+        contrasenaActual: [
+          '',
+          [Validators.required, Validators.pattern(this.passwordPattern)],
+        ],
+        contrasenaNueva: [
+          '',
+          [Validators.required, Validators.pattern(this.passwordPattern)],
+        ],
+        contrasenaRepetida: [
+          '',
+          [Validators.required, Validators.pattern(this.passwordPattern)],
+        ],
+      },
+      {
+        validators: this.checkPasswordsMatch,
+      }
+    );
+
+    // Reactivar validaciones dinámicas y limpieza de errores al escribir
+    const nueva = this.editPassword.get('contrasenaNueva');
+    const repetida = this.editPassword.get('contrasenaRepetida');
+
+    if (nueva && repetida) {
+      this.subs.push(
+        nueva.valueChanges.subscribe(() => {
+          this.editPassword.updateValueAndValidity({
+            onlySelf: false,
+            emitEvent: false,
+          });
+        })
+      );
+      this.subs.push(
+        repetida.valueChanges.subscribe(() => {
+          this.editPassword.updateValueAndValidity({
+            onlySelf: false,
+            emitEvent: false,
+          });
+        })
+      );
+    }
+  }
+
+  clickEvent(field: string, event: MouseEvent) {
+    if (this.hidePassword.hasOwnProperty(field)) {
+      this.hidePassword[field] = !this.hidePassword[field];
+    }
+    event.stopPropagation();
+  }
+
+  checkPasswordsMatch(group: AbstractControl | FormGroup | null): { [key: string]: any } | null {
+    if (!group) return null;
+    const pass = group.get('contrasenaNueva')?.value;
+    const repeat = group.get('contrasenaRepetida')?.value;
+
+    if (pass === undefined || repeat === undefined) {
+      return null;
     }
 
-    clickEvent(field: string, event: MouseEvent) {
-      if (this.hidePassword.hasOwnProperty(field)) {
-        this.hidePassword[field] = !this.hidePassword[field];
-      }
-      event.stopPropagation();
-    }
-
-    checkPasswordsMatch(group: FormGroup) {
-      let passControl = group.get('contrasenaNueva');
-      let confirmPassControl = group.get('contrasenaRepetida');
-  
-      if (passControl && confirmPassControl) {
-        let pass = passControl.value;
-        let confirmPass = confirmPassControl.value;
-        return pass === confirmPass ? null : { notSame: true }
-      }
-      return { notSame: true };
-    }
+    return pass === repeat ? null : { notSame: true };
+  }
 
   checkPasswords() {
-    if (this.userModify.id != null) {
-      this.service.checkPasswords(this.userModify.id, this.editPassword.get("contrasenaActual")?.value).subscribe(response => {
-        if (response == true) {
-          let usuario: Usuario = {id: this.userModify.id,
-            email: this.userModify.email,
-            password: this.userModify.password,
-            apellidos: this.userModify.apellidos,
-            partidosAsistidos: this.userModify.partidosAsistidos,
-            nombre: this.userModify.nombre,
-            _admin: this.userModify._admin}
+    if (!this.editPassword.valid) return;
 
-          usuario.password = this.editPassword.get("contrasenaNueva")?.value;
-          this.service.modifyUser(usuario).subscribe(() => { })
-          this.closedModal();
-        } else {
-          alert("La contraseña actual no existe");
-        }
-      })
-    }
-    else {
-      alert("Id usuario indefinido");
+    if (this.userModify?.id) {
+      this.service
+        .checkPasswords(
+          this.userModify.id,
+          this.editPassword.get('contrasenaActual')?.value
+        )
+        .subscribe((response) => {
+          if (response === true) {
+            const usuario: Usuario = {
+              id: this.userModify.id,
+              email: this.userModify.email,
+              password: this.editPassword.get('contrasenaNueva')?.value,
+              apellidos: this.userModify.apellidos,
+              partidosAsistidos: this.userModify.partidosAsistidos,
+              nombre: this.userModify.nombre,
+              _admin: this.userModify._admin,
+            };
+
+            this.service.modifyUser(usuario).subscribe(() => {
+              this.closedModal();
+            });
+          } else {
+            alert('La contraseña actual no es correcta');
+          }
+        });
+    } else {
+      alert('Id usuario indefinido');
     }
   }
 
@@ -103,7 +159,7 @@ export class EditPasswordComponent {
     this.dialogRef.close();
   }
 
-
+  ngOnDestroy() {
+    this.subs.forEach((s) => s.unsubscribe());
+  }
 }
-
-
